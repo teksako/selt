6e7c5b6e26cba601821d5f24c72dac99a26466e5
teksako <teksako@op.pl>
2021-07-01 22:29:06 +0200

    start

---
+++ src/main/java/com/selt/model/Employee.java
@@ -0,0 +1,4 @@
+package com.selt.model;
+
+public class Employee {
+}
---
+++ src/test/java/com/selt/SeltApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeltApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
